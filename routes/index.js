6cc75e467e353a318a7a02a5b40a8973208e16fd
sebasss18 <sebasbernals11@gmail.com>
2026-05-09 11:17:27 -0700

    end of assignment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('kaka');
+    res.send('Hello');
 });
+
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
